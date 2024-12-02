297444fe4317d65a3914a83071a2b234551240a0
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-12-02 15:48:23 +0600

    make user odel and make register page and add it db with bcrypt

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const app = express()
+const userModel = require('./models/user.model');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
-app.set('view engine', "ejs")
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', "ejs") // for view engine
+app.use(express.urlencoded({ extended: true })) // for get form data
 
-app.get('/', (req, res) => {
+app.use(express.json())  //JSON ডেটা পার্স করার জন্য Middleware
+app.use(cookieParser()) //for read cookies
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    console.log(req.body);
+    const { username, name, age, email, password } = req.body;
+    const user = await userModel.findOne({ email })
+    console.log(user);
+
+    if (user) {
+        return res.status(300).status({
+            message: 'User Already Registered'
+        })
+    }
+    console.log(password);
+
+    bcrypt.genSalt(10, function (err, salt) {
+        console.log(salt);
+        bcrypt.hash(password, salt, async function (err, hash) {
+            // Store hash in your password DB.
+            const user = await userModel.create({
+                username,
+                name,
+                age,
+                email,
+                password: hash
+            })
+            
+            res.send(user)
+        });
+    });
+
+})
+
+app.post('/create', (req, res) => {
     res.send('Hello World')
 })
 
--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/minProject')
 
 const userSchema = mongoose.Schema({
-
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    post: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        }
+    ]
 })
 
 
